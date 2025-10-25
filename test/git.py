c3ef77273e7854b75204fd908587996fd8f0d7eb
Sparsh720 <tekariraj040@gmail.com>
2025-10-25 03:24:52 +0000

    second commit

--- test/git.py
+++ test/git.py
@@ -1,3 +1,9 @@
 print("hi this is delta force operator speaking")
 print("your code is 33223")
 print("this is your captain speaking")
+
+a = int(input("enter a number: "))
+b = int(input("enter second number: "))
+c = a + b
+print("the sum is: ", c)
+print("end of code")
